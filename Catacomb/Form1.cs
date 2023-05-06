e6f3468aea6d79ce86f5d1fde8c5f63a156d17c4
MarsheeMellow <104988392+MitchellCWM@users.noreply.github.com>
2023-05-06 19:19:40 -0300

    starting, hopefully

---
+++ Catacomb/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Catacomb
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            
+            InitializeComponent();
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Size = new System.Drawing.Size(500, 500);
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+    }
+}
